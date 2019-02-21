d9ac088179958ac681c0c5ad02beb252566a0ea8
Izabella <izzwii.izabella@gmail.com>
2019-02-21 16:04:10 +0100

    stared with the story

--- bandersnatch/Program.cs
+++ bandersnatch/Program.cs
@@ -6,7 +6,16 @@ namespace bandersnatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("The people of Winerfall has requested your help to enter the big and scary cave. You accept the quest.\nOutside you meet a man, ha warns you to enter and ask you who you are. ");
+
+            var start = new StoryStart();
+            start.whoAreYou();
+
+            Console.WriteLine();
+            Console.WriteLine("You decide to enter the cave without knowing if you'll ever come out of it...");
+
         }
     }
 }
---
+++ bandersnatch/StoryStart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace bandersnatch
+{
+    public class StoryStart
+    {
+        public StoryStart()
+        {
+
+        }
+
+        public void whoAreYou()
+        {
+            var user = "human";
+
+            while (user == "human")
+            {
+                Console.WriteLine("\"Are you a Wizard or a Barbarian?\"- Werd man");
+                Console.Write(">");
+                var typeOfUser = Console.ReadLine().ToLower();
+                if (typeOfUser == "wizard" || typeOfUser == "barbarian")
+                {
+                    user = "hej";
+                    Console.WriteLine("\"What's your name?\" - Werd man");
+                    Console.Write(">");
+                    var userName = Console.ReadLine();
+                    Console.WriteLine($"Hello {typeOfUser} {userName}");
+                    Console.WriteLine();
+                    Console.WriteLine("\"I fear this is a very scary cave, you might never return\" - Werd man");
+                }
+                else
+                {
+                    Console.WriteLine("\"As werd as you look you must be someting else then a human\" - Werd man" );
+                }
+            }
+        }
+    }
+}
