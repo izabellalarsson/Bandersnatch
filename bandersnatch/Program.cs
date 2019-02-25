c9c144e6781c341f13a6332754e2ec025b0ee685
Izabella <izzwii.izabella@gmail.com>
2019-02-25 15:30:05 +0100

    Update Program.cs

--- bandersnatch/Program.cs
+++ bandersnatch/Program.cs
@@ -5,8 +5,6 @@ namespace bandersnatch
     class Program
     {
         static void Main(string[] args)
-
-
         {
             Console.WriteLine("The people of Winerfall has requested your help to enter the big and scary cave. You accept the quest.\nOutside you meet a man, ha warns you to enter and ask you who you are. ");
 
