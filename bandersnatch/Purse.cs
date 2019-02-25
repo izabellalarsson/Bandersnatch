d338ab93814ee2749f0d8db662dec200d2ca2957
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-02-25 15:43:41 +0100

    fixed the broken thing

---
+++ bandersnatch/Purse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace bandersnatch
+{
+    public class Purse
+    {
+        public Purse()
+        {
+        }
+
+        public void PickUpPurse()
+        {
+            var notChosen = false;
+            var choice = new yesOrNo();
+
+            while (!notChosen)
+            {
+                Console.WriteLine("Should you take a look? [yes]/[no]");
+                Console.Write("> ");
+                var userInput = Console.ReadLine().ToLower();
+                if (userInput == "yes")
+                {
+                    notChosen = true;
+                    choice.yes();
+                    choice.YesNo();
+
+                }
+                else if (userInput == "no")
+                {
+                    notChosen = true;
+                    choice.no();
+                    choice.YesNo();
+                }
+            }
+        }
+    }
+}
--- bandersnatch/StoryStart.cs
+++ bandersnatch/StoryStart.cs
@@ -3,10 +3,7 @@ namespace bandersnatch
 {
     public class StoryStart
     {
-<<<<<<< Updated upstream
-=======
         public bool HaveReceipt { get; set; }
->>>>>>> Stashed changes
         public StoryStart()
         {
 
@@ -18,39 +15,23 @@ namespace bandersnatch
 
             while (user == "human")
             {
-<<<<<<< Updated upstream
                 Console.WriteLine("\"Are you a Wizard or a Barbarian?\"- Werd man");
-=======
-                Console.WriteLine("\"Are you a Wizard or a Barbarian?\"- Weird man");
->>>>>>> Stashed changes
                 Console.Write(">");
                 var typeOfUser = Console.ReadLine().ToLower();
                 if (typeOfUser == "wizard" || typeOfUser == "barbarian")
                 {
                     user = "hej";
-<<<<<<< Updated upstream
-                    Console.WriteLine("\"What's your name?\" - Werd man");
-=======
                     Console.WriteLine("\"What's your name?\" - Weird man");
->>>>>>> Stashed changes
                     Console.Write(">");
                     var userName = Console.ReadLine();
                     Console.WriteLine($"Hello {typeOfUser} {userName}");
                     Console.WriteLine();
-<<<<<<< Updated upstream
-                    Console.WriteLine("\"I fear this is a very scary cave, you might never return\" - Werd man");
-                }
-                else
-                {
-                    Console.WriteLine("\"As werd as you look you must be someting else then a human\" - Werd man" );
-=======
                     Console.WriteLine($"\"I fear this is a very scary cave and dark cave you might never return. Here you go little {typeOfUser}\" - Weird man");
                     Console.WriteLine("The Weird man hands over a flashlight to you");
                 }
                 else
                 {
                     Console.WriteLine("\"As werd as you look you must be someting else then a human\" - Weird man" );
->>>>>>> Stashed changes
                 }
             }
         }
